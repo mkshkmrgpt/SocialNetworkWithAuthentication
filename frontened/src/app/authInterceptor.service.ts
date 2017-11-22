25ff0de5b75eec26b3e00b131a00598c93e455f7
Mukesh kumar gupta <mkshkmrgpt@gmail.com>
2017-11-22 23:01:59 +0530

    store autherization token in request headers

--- frontened/src/app/authInterceptor.service.ts
+++ frontened/src/app/authInterceptor.service.ts
@@ -4,8 +4,10 @@ import { HttpInterceptor, HttpHandler, HttpRequest  } from "@angular/common/http
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor{
 
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log(req)
-        return next.handle(req)
+    intercept(req, next) {
+        var authRequest = req.clone({
+            headers : req.headers.set('Authorization', 'token '+'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YTBmZmM3M2Y1NGQyMDJlZjgxMTM4OWQifQ.2RMIs1qHOVv2nZIysNC_09rGJ8GfgmKvR-5i8PwaevM')
+        })
+        return next.handle(authRequest)
     }
 }
